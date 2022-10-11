9ae25af2a7458b03cab984eee9d7d2ec1c60f1ea
zVqnom <101054531+zVqnom@users.noreply.github.com>
2022-10-11 10:02:13 +0700

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,20 +1,9 @@
 const {Client} = require('discord.js');
-const mongoose = require('mongoose');
-const config = require("../../config.json");
 
 module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
-        await mongoose.connect(config.mongodb || '', {
-            keepAlive: true,
-        });
-
-        if (mongoose.connect) {
-            console.log('MongoDB connection succesful.')
-        }
-
         console.log(`${client.user.username} is now online.`);
-
     },
 };
